882441e0f598bf078b1997680134550460aa4d31
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-10-27 16:40:01 +0530

    MongoDB connectivity checked

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 
