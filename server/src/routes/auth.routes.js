323c1e5a7dadce21573ff9badd3759a1b0068c82
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-10-28 17:01:45 +0530

    auth route corrected, user schema updated

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 
-import { loginV1, loginOAuth, signupV1, logout} from './controller/authController'
+import { loginV1, signupV1, logout, success, error } from './controller/authController';
+import { authenticate } from passport;
 
 const AuthRouter = Router();
 
 AuthRouter.post('/login/v1', loginV1);
-AuthRouter.post('/login/OAuth', loginOAuth)
+AuthRouter.get('/login/OAuth', authenticate('google', { scope: ['profile', 'email'] }));
+AuthRouter.get('/login/OAuth/callback', authenticate('google', { successRedirect: '/auth/success', failureRedirect: '/auth/error' }))
 AuthRouter.post('/signup/v1', signupV1);
 //AuthRouter.post('/signup/Oauth', signupOAuth);
 AuthRouter.post('/logout', logout);
+
+AuthRouter.get('/success', success);
+AuthRouter.get('/error', error);
 
 export default {
     AuthRouter
