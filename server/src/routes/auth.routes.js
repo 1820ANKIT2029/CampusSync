12f8a9a7d2bb78990f034e4e71799ac45421a70f
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-10-28 20:43:28 +0530

    route updated

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -10,7 +10,7 @@ const AuthRouter = Router();
 AuthRouter.post('/login/v1', loginV1);
 AuthRouter.get('/login/OAuth', authenticate('google', { scope: ['profile', 'email'] }));
 AuthRouter.get('/login/OAuth/callback', authenticate('google', { successRedirect: '/auth/success', failureRedirect: '/auth/error' }))
-AuthRouter.post('/signup/v1', signupV1);
+AuthRouter.post('/signup/v1', authenticate('local'), signupV1);
 // AuthRouter.post('/signup/Oauth', signupOAuth);
 AuthRouter.post('/logout', logout);
 
