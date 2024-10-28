80d0d18cf40bc89a3c9b88cd7b58c58571e4cdad
aloknegi786 <aloksinghnegi005@gmail.com>
2024-10-28 16:25:17 +0530

    adding strategies

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import passport from 'passport';
+import "../util/passport.Google.js"
 
 import { loginV1, loginOAuth, signupV1, logout} from './controller/authController'
 
@@ -7,7 +9,7 @@ const AuthRouter = Router();
 AuthRouter.post('/login/v1', loginV1);
 AuthRouter.post('/login/OAuth', loginOAuth)
 AuthRouter.post('/signup/v1', signupV1);
-//AuthRouter.post('/signup/Oauth', signupOAuth);
+// AuthRouter.post('/signup/Oauth', signupOAuth);
 AuthRouter.post('/logout', logout);
 
 export default {
