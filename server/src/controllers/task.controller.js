5789fd59bece97a25907ca2915741d2bf3382621
aloknegi786 <aloksinghnegi005@gmail.com>
2024-10-30 01:58:03 +0530

    remove event and task function corrected

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -1,4 +1,4 @@
-import Task from "../models/task.models.js";
+import {Task} from "../models/task.models.js";
 
 export const createTask = async (req,res,next) => {
     const {
@@ -6,11 +6,10 @@ export const createTask = async (req,res,next) => {
         name,
         description,
         dueDate,
-        isCompleted
     }  = req.body;
 
     try{
-        const exist = await Task.findOne({name});
+        const exist = await Task.findOne({name,eventId});
 
         if(exist){
             return res.status(409).json({error: "task already present"});
@@ -21,7 +20,6 @@ export const createTask = async (req,res,next) => {
             name,
             description,
             dueDate,
-            isCompleted
         })
 
         const task = newTask.save();
@@ -35,16 +33,19 @@ export const createTask = async (req,res,next) => {
 export const removeTask = async (req,res,next) => {
     const {
         id
-    } = req.params;
+    } = req.query;
+    console.log(id);
 
     try{
-        const exist = Task.findById({id});
+        const exist = Task.findById({_id:id});
 
         if(!exist){
             return res.status(404).json({error: "task not found"});
         }
 
-        await Task.findByIdAndDelete(id);
+        console.log(exist.toString());
+
+        await Task.findByIdAndDelete({_id: id});
         return res.status(200).json({error: "task deleted successively"});
     }catch(error){
         return res.status(500).json({error: "Internal server error in remove task."})
