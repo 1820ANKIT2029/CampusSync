a1126d0cf24065a93601746e6e91e09322deb4cf
aloknegi786 <aloksinghnegi005@gmail.com>
2024-10-30 02:04:02 +0530

    changed hashPassword function to async function type

--- server/src/util/hash_function.js
+++ server/src/util/hash_function.js
@@ -4,10 +4,10 @@ import bcrypt from 'bcrypt';
 const saltRounds = 10;
 
 
-export const hashPassword =  (password) => {
-    const salt = bcrypt.genSaltSync(saltRounds);
-    return bcrypt.hashSync(password, salt);
-};
+export const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(saltRounds); 
+    return await bcrypt.hash(password, salt); 
+  };
 
 
 export const comparePassword = (plain, hashed) => {
