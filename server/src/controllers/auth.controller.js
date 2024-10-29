ff6519b84a00c1ca7f2c3dce7eb31f474b939af5
aloknegi786 <aloksinghnegi005@gmail.com>
2024-10-29 14:27:17 +0530

    some major changes in auth.controller.

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,1 +1,100 @@
+import passport from 'passport';
 
+import { User, Profile } from '../models/user.models.js';
+import { hashPassword } from "../util/hash_function.js";
+
+export const loginV1 =  passport.authenticate("local", {
+    successRedirect: "/auth/success",
+    failureRedirect: "/auth/error",
+  })
+
+export const signupV1 = async (req, res, next) => {
+    const {username,password,confirmPassword,email} = req.body;
+
+    const user = await User.findOne({username});
+
+    if(user){
+        return res.status(400).json({error: "User already exists"});
+    }
+
+    if(password != confirmPassword){
+        return res.status(400).json({error: "Password doesn't match"});
+    }
+
+    const newUser = new User({
+        username,
+        password: hashPassword(password),
+        email,
+    })
+
+    if(newUser){
+        await newUser.save();
+
+        const newprofile = new Profile({
+            userid: newUser._id,
+            email,
+        })
+
+        const new_profile = await newprofile.save();
+
+        res.status(201).json({
+            _id:newUser._id,
+            username,
+            email,
+        })
+    }
+    else{
+        res.status(400).json({error: "Invalid User Data"});
+    }
+}
+
+export const googleLogin = passport.authenticate('google', { scope: ['profile', 'email']})
+
+export const googleLoginafter = (req, res, next) => {
+        if(req.user){
+            res.status(400).json({error: "Invalid User!"});
+        }else{
+            res.status(200).json({message: "valid User!"});
+        }
+}
+
+export const googlecallback = passport.authenticate('google',
+    { 
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/error'
+    }
+);
+
+export const logout = (req, res, next) => {
+    if(req.isAuthenticated()){
+        req.logout(function (err) {
+            if (err) {
+              return next(err);
+            }
+            req.session.destroy(() => {
+              res.clearCookie("connect.sid"); // Clear session cookie (optional)
+              res.status(200).json({message: "logout successfully"});
+            });
+        });
+    }
+    else{
+        res.status(401).json({error: "already logout"});
+    }
+    
+  }
+
+export const success = (req, res, next) => {
+    res.status(200).json(
+        {
+            message: `Login successful! Welcome ${req.user.email}`
+        }
+    );
+}
+
+export const error = (req, res, next) => {
+    res.status(400).json(
+        {
+            error: `Login failed! Please try again.`
+        }
+    );
+}
--- server/src/strategies/google-strategy.js
+++ server/src/strategies/google-strategy.js
@@ -1,56 +1,51 @@
-import passport from "passport";
 import GoogleStrategy from "passport-google-oauth2";
-import models from '../models/user.models.js'; // Adjust the path
-const { User, Profile, Image } = models;
+import {User, Profile} from '../models/user.models.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
-// Configure serialization
-passport.serializeUser((user, done) => {
-    return done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const findUser = await User.findById(id);
-        return findUser ? done(null, findUser) : done(null, null);
-    } catch (err) {
-        return done(err, null);
-    }
-});
 
 // Set up Google OAuth strategy
-passport.use(
-    "google",
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK,    
-            scope: ["profile", "email"],
-        },
-        async (accessToken, refreshToken, profile, done) => {
-            try {
-                // Extract email from the Google profile
-                const email = profile.emails[0].value;
-                let findUser = await User.findOne({ email });
-                
-                if (!findUser) {
-                    const newUser = new User({
-                        username: profile.id,
-                        email: email,
-                        password: "Google",
-                    });
-                    const newSavedUser = await newUser.save();
-                    return done(null, newSavedUser);
-                }
-                
-                return done(null, findUser);
-            } catch (err) {
-                return done(err, null);
+const googlestrategy = new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK,    
+        scope: ["profile", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            // Extract email from the Google profile
+            // if (profile.domain !== process.env.COLLEGE_DOMAIN) {
+            //     return done(null, false, { message: "Unauthorized domain" });
+            // }
+            const email = profile.emails[0].value;
+            let findUser = await User.findOne({ email });
+            
+            if (!findUser) {
+                const newUser = new User({
+                    username: profile.id,
+                    email: email,
+                    password: "Google",
+                });
+                const newSavedUser = await newUser.save();
+
+                const newprofile = new Profile({
+                    userid: newUser._id,
+                    name: profile.displayName,
+                    profilePic: profile.photos[0].value,
+                    email,
+                })
+
+                const new_profile = await newprofile.save();
+                return done(null, newSavedUser);
             }
+            
+            return done(null, findUser);
+        } catch (err) {
+            return done(err, null);
         }
-    )
+    }
 );
 
-export default passport;
+
+export default googlestrategy;
