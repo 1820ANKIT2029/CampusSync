b8cd39841ed5f6a046f947b58f3ba6e0e00885e9
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-11-02 02:31:37 +0530

    more routes added

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { ensureLoggedIn } from '../middleware/protectRoute.js';
-import { profile } from '../controllers/user.controller.js';
+import { event, profile } from '../controllers/user.controller.js';
 
 
 const UserRouter = Router();
 
-UserRouter.get('/event',ensureLoggedIn );
+UserRouter.get('/event', ensureLoggedIn, event );
+UserRouter.get('/event/active', ensureLoggedIn);
+UserRouter.get('/active/inactive', ensureLoggedIn)
 UserRouter.get('/profile',ensureLoggedIn , profile);
+UserRouter.post('/profile/edit', ensureLoggedIn);
 
 
 export default UserRouter;
