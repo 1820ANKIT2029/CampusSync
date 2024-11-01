d5d9086d761b50ae498594ac05769b17ecc82703
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-11-02 02:28:38 +0530

    get event of user function add

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { EventParticipant } from "../models/event.model.js";
 import { Profile } from "../models/user.models.js"
 import { error } from "./auth.controller.js";
 
@@ -9,4 +10,19 @@ export const profile = async (req, res, next) => {
     }
 
     return res.status(200).json(result);
+}
+
+export const event = async (req, res, next) => {
+    const id = req.user.id;
+    try{
+        const profile = await Profile.findOne({userid: id});
+        const result = await EventParticipant.find({participantId: profile._id}).populate('eventId');
+        if(!result){
+            return res.status(404).json({error: "unable to fetch events"});
+        }
+        return res.status(200).json(result);
+
+    }catch(err){
+        return res.status(500).json({ error: "Internal server error at user events" });
+    }
 }
