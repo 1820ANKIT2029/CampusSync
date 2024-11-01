1dad62870ca42d98f81ce38c6c5a51c5303a6966
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-01 21:26:53 +0530

    linked backend login functionality with client

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 function Login() {
     const [values,setValues] = useState({
@@ -7,18 +8,33 @@ function Login() {
         password: "",
     })
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(values);
-        axios.post('http://localhost:3000/auth/login/v1',values)
-        .then((response) => {
-            console.log(response);
-        })
-        setValues({
-            username:"",
-            password:"",
-        })
-    }
+    const navigate = useNavigate();
+
+    const loginWithLocal = async (e) => {
+      e.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:3000/auth/login/v1",values, {
+                withCredentials: true 
+            });
+    
+            if (response.status === 200) {
+                console.log(response)
+                console.log("login successively");
+                navigate('/')
+            }
+            else{
+              console.log(response);
+            }
+        } catch (error) {
+            console.error("Login failed:", error);
+            console.log("unable to login");
+        }
+    };
+    
+
+  const loginwithgoogle = ()=>{
+      window.open("http://localhost:3000/auth/login/OAuth/callback","_self")
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
@@ -26,7 +42,7 @@ function Login() {
         <h2 className="text-center text-lg font-semibold mb-6">Log in with</h2>
         
         <div className="flex justify-between mb-6">
-          <button className="bg-white text-black w-full py-2 rounded-md mr-2 flex items-center justify-center">
+          <button onClick={loginwithgoogle} className="bg-white text-black w-full py-2 rounded-md mr-2 flex items-center justify-center">
             <img src="https://img.icons8.com/color/24/000000/google-logo.png" alt="Google" className="mr-2"/>
             Google
           </button>
@@ -34,7 +50,7 @@ function Login() {
         
         <p className="text-center text-sm text-gray-400 mb-4">or</p>
         
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={loginWithLocal}>
           <input
             type="text"
             placeholder="your username"
