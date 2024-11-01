a8a2fab5de1ccc5c6be600b29cc0fb36347d5589
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-01 11:33:03 +0530

    added news controller & routes

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ensureLoggedIn } from '../middleware/protectRoute.js';
 import { createTask,removeTask } from '../controllers/task.controller.js';
 import { createEvent, removeEvent } from '../controllers/event.controller.js';
+import { addNews, removeNews } from '../controllers/news.controller.js';
 
 const AdminRouter = Router();
 
@@ -12,5 +13,9 @@ AdminRouter.post('/event/task/create',ensureLoggedIn ,createTask);
 AdminRouter.delete('/event/delete',ensureLoggedIn, removeEvent);
 
 AdminRouter.delete('/event/task/delete',ensureLoggedIn ,removeTask);
+
+AdminRouter.post('/news/create',ensureLoggedIn,addNews);
+
+AdminRouter.delete('/news/delete',ensureLoggedIn,removeNews);
 
 export default AdminRouter;
