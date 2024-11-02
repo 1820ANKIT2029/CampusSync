1ca197fd8fcc0d404fd6551ef794c7e5bee94e70
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-02 10:05:11 +0530

    added protectRoute feature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
+
 
 
 import Layout from './Layout.jsx'
-// import Home from './components/Home/Home.jsx'
-// import About from './components/About/About.jsx'
-// import Contact from './Contact/Contact.jsx'
-// import User from './components/User/User.jsx'
-// import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 import './index.css'
 import Login from './components/login/login.jsx'
 import Home from './components/Home.jsx'
 import Signup from './components/signup/Signup.jsx'
 import Test from './components/Test.jsx'
+import ProtectedRoute from './ProtectedRoute.jsx'
 
 
 const router = createBrowserRouter(
@@ -22,21 +21,20 @@ const router = createBrowserRouter(
         <Route path="" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
-        <Route path="/test" element={<Test/>}/>
-        {/* <Route path="about" element={<About/>} /> */}
-        {/* <Route path="contact" element={<Contact/>} /> */}
-        {/* <Route path="user/:userid" element={<User/>} /> */}
-        {/* <Route
-         loader={githubInfoLoader}
-         path="github" 
-         element={<Github/>} 
-         /> */}
+        <Route
+        path="/test"
+        element={
+          <ProtectedRoute>
+            <Test />
+          </ProtectedRoute>
+        }
+      />
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+  <Provider store={store}>
+      <RouterProvider router={router}/>
+  </Provider>,
 )
