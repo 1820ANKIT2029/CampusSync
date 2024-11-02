6a74e57aed3dc23171cacc2d6f892f2abf108236
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-02 10:07:27 +0530

    removed local checking of isLogged in

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, NavLink } from 'react-router-dom';
+import { setAuth } from '../features/authentication/authSlice';
 
 function Home() {
+  const isAuthenticated = useSelector(state => state.isAuthenticated)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
   const handleClick = async (e) => {
-    e.preventDefault(); // Prevent default behavior, if necessary
+    e.preventDefault();
 
     try {
       const result = await axios.post('http://localhost:3000/auth/logout', {}, { withCredentials: true });
-      console.log(result);
+      dispatch(toggleAuth(false))
     } catch (error) {
       console.error("Error during logout:", error);
       alert("An error occurred while logging out.");
@@ -22,6 +29,7 @@ function Home() {
       >
         Logout
       </button>
+      <NavLink to='/test' className='w-full bg-purple-600 py-2 rounded-md hover:bg-purple-700 transition-colors'>test</NavLink>
     </div>
   );
 }
