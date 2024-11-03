41ae2a8bb8a3817a035cb76bc3281832bbc0a781
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-11-03 18:28:57 +0530

    CRED routes for admin defined events added

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -3,6 +3,14 @@ import { ensureLoggedIn, ensureAdmin } from '../middleware/protectRoute.js';
 import { createTask,removeTask } from '../controllers/task.controller.js';
 import { createEvent, removeEvent } from '../controllers/event.controller.js';
 import { addNews, removeNews } from '../controllers/news.controller.js';
+import { 
+    adminEvents, 
+    adminEventById, 
+    adminActiveEvents, 
+    adminInactiveEvents,
+    adminNews,
+    adminNewsById
+} from '../controllers/admin.controller.js';
 
 const AdminRouter = Router();
 
@@ -13,7 +21,15 @@ AdminRouter.put('/event/task/create', createTask);
 AdminRouter.delete('/event/delete', removeEvent);
 AdminRouter.delete('/event/task/delete', removeTask);
 AdminRouter.post('/news/create', addNews);
+AdminRouter.put('/news/edit/:newsId', EditNews);
 AdminRouter.delete('/news/delete', removeNews);
-AdminRouter.put('/submission/verify/:submissionId', verifySubmission)
+AdminRouter.put('/submission/verify/:submissionId', verifySubmission);
+
+AdminRouter.get('/event', adminEvents);
+AdminRouter.get('/event/details/:eventId', adminEventById);
+AdminRouter.get('/event/active', adminActiveEvents);
+AdminRouter.get('/event/inactive', adminInactiveEvents);
+AdminRouter.get('/news', adminNews);
+AdminRouter.get('/news/:newsId', adminNewsById)
 
 export default AdminRouter;
