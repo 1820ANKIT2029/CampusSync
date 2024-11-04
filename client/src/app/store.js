59500598ef51e4c173206c890e6890033307694d
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-04 21:41:33 +0530

    added reducers for home page

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import authReducer from '../features/authentication/authSlice.js'
+import eventReducer from '../features/HomePageSlice/eventSlice.js'
 import { configureStore } from '@reduxjs/toolkit';
 
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  event:eventReducer,
 });
 
 export const store = configureStore({
