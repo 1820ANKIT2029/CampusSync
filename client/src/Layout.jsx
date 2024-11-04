457f4d3a4a6a5db0f1b7ee2a95aaf29a98876807
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-04 22:17:20 +0530

    added navbar and footer to the main layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-// import Header from './components/Header/Header.jsx'
-// import Footer from './components/Footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function Layout() {
   return (
     <>
-      {/* <Header/> */}
-       <Outlet/>
-
-      {/* <Footer/> */}
+      <div className="overflow-hidden w-full min-h-screen p-4 bg-blue-100 transition-color">
+        <div className="max-w-screen-xl mx-auto ">
+          <Navbar/>
+          <Outlet/>
+          <div className="w-full h-px bg-gray-400 my-6" />
+          <Footer/>
+        </div>
+      </div>
     </>
   )
 }
