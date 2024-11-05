2e73de2d1bd39c813e4d356f28004a6308c46b30
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-11-05 23:30:57 +0530

    Improved the CRED News controller

--- server/src/controllers/news.controller.js
+++ server/src/controllers/news.controller.js
@@ -2,52 +2,60 @@ import { News } from "../models/news.model.js";
 import { Profile } from "../models/user.models.js";
 
 export const addNews = async (req,res,next) => {
-    const { adminId, headline, description, date } = req.body;
+    const { headline, description, date } = req.body;
+    const id = req.user.id;
+
+    if(!(headline && description && date)){
+        return res.status(400).json({error: " headline, description, date are not provided"});
+    }
+
+    const DATE = new Date(date);
 
     try{
-        const exist = await News.findOne({adminId,headline,date});
+        const profile = await Profile.findOne({userid: id}).select('_id');
+        const exist = await News.findOne({
+            adminId: profile._id,
+            headline: headline
+        });
         if(exist){
             res.status(400).json({error: "News already exist"});
         }
 
         const newNews = new News({
-            adminId,
+            adminId: profile._id,
             headline,
             description,
-            date,
+            date: DATE,
         })
 
         await newNews.save();
 
         return res.status(200).json({message: "news created"});
     }catch(error){
-        console.log(error);
         res.status(500).json({error: "Internal Server Error in addNews"});
     }
 }
 
-export const removeNews = async (req,res,next) => {
-    const {_id, adminId} = req.body;
+export const removeNews = async (req, res, next) => {
+    const { newsId } = req.params;
+    const id = req.user.id;
     
     try{
-        const news = await news.findById({_id});
+        const profile = await Profile.findOne({userid: id}).select('_id');
+        const result = await News.deleteOne({
+                _id: newsId,
+                adminId: profile._id
+        });
 
-        if(!news){
-            res.status(404).json({error: "news not found"});
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: "News not found" });
         }
 
-        if(news.adminId != adminId){
-            res.status(404).json({error: "you are not authorized to delete the news"});
-        }
-
-        await News.deleteOne({_id});
-        
-        res.status(200).json({message: "news deleted!"});
+        res.status(200).json({ message: "News deleted successfully" });
     }catch(error){
         res.status(500).json({error: "Internal server error found in removeNews"});
     }
 }
-
 
 export const EditNews = async (req, res, next) => {
     const { newsId } = req.params;
@@ -72,7 +80,7 @@ export const EditNews = async (req, res, next) => {
         if(!news){
             return res.status(400).json({error: "News update failed"});
         }
-        return res.status(204).json({message: "News updated successfully"});
+        return res.status(200).json({message: "News updated successfully"});
     }catch(err){
         res.status(500).json({error: "Internal server error found in edit news"});
     }
