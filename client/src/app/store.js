6621981819f39c563b42dd39a0b4408f458ef234
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-06 18:12:42 +0530

    handled authentication related bugs

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import authReducer from '../features/authentication/authSlice.js'
 import eventReducer from '../features/HomePageSlice/eventSlice.js'
+import adminReducer from '../features/isAdmin/adminSlice.js'
 import { configureStore } from '@reduxjs/toolkit';
 
 
@@ -8,6 +9,7 @@ import { configureStore } from '@reduxjs/toolkit';
 const rootReducer = combineReducers({
   auth: authReducer,
   event:eventReducer,
+  admin:adminReducer,
 });
 
 export const store = configureStore({
