27dd1df58a6c92f975275e448b84bd631f706e77
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-06 18:14:41 +0530

    added admin related reducers

---
+++ client/src/features/isAdmin/adminSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isAdmin: true,
+}
+
+export const adminSlice = createSlice({
+    name: 'admin',
+    initialState,
+    reducers: {
+        setAdmin: (state, action) => {
+            if (typeof action.payload === 'boolean') {
+                state.isAdmin = action.payload;
+            } else {
+                console.error("Payload for setAuth should be a boolean");
+            }
+        },
+    },
+});
+
+export const { setAdmin } = adminSlice.actions;
+
+export default adminSlice.reducer;
