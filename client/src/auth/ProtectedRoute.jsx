37dc689ee1a3effb513d50bcb60d01a94292e74a
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-06 18:16:00 +0530

    created separeate folder for auth related files

---
+++ client/src/auth/ProtectedRoute.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setAuth } from '../features/authentication/authSlice';
+import Cookies from 'js-cookie';
+import { setAdmin } from '../features/isAdmin/adminSlice';
+
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const myCookie = Cookies.get('connect.sid');
+    if (myCookie) {
+      dispatch(setAuth(true));
+    } else {
+      dispatch(setAuth(false));
+      dispatch(setAdmin(false));
+      navigate('/');
+    }
+  }, [dispatch, navigate]);
+
+  return isAuthenticated ? children : null;
+};
+
+export default ProtectedRoute;
