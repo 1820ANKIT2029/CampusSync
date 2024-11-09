9d5dd5cf389f8448847955bf68bb882a21aaad8a
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2024-11-09 20:43:35 +0530

    notification socket

--- server/src/SecondServer.js
+++ server/src/SecondServer.js
@@ -17,38 +17,48 @@ dotenv.config();
 
 const PORT = process.env.SECOND_PORT;
 
-const app = express();
-const server = http.createServer(app);
+const StartServer = async () => {
+    try{
+        await connectToMongoDB();
 
-const io = new Server(server, {
-    cors: {
-        origin: '*',
+        const app = express();
+        const server = http.createServer(app);
+
+        const io = new Server(server, {
+            cors: {
+                origin: '*',
+            }
+        });
+
+        // this cron is responsive for aura distribution after the end of events
+        // run every hour
+        cron.schedule('* 1 * * *', updateGlobalAura);
+        cron.schedule('59 23 * * *', deleteOldNotification);
+
+        const notification = io.of("notification");
+        const leaderBoard = io.of("leaderBoard");
+        const eventLeaderboard = io.of("eventLeaderboard");
+        //const chatRoom = io.of("chatRoom");
+
+        // auth of socket
+        //chatRoom.use(CheckTokenInSocket);
+        notification.use(CheckTokenInSocket);
+
+        leaderBoard.on('connection', GlobalLeaderBoard);
+        eventLeaderboard.on('connection', LocalLeaderBoard);
+        notification.on('connection', NotificationSocket);
+
+        app.get('/', (req, res) => {
+            res.send('Notification server is running.');
+        });
+
+        server.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+
+    }catch(err){
+        console.log(err);
     }
-});
+};
 
-// this cron is responsive for aura distribution after the end of events
-// run every hour
-cron.schedule('* 1 * * *', updateGlobalAura);
-cron.schedule('59 23 * * *', deleteOldNotification);
-
-const notification = io.of("notification");
-const leaderBoard = io.of("leaderBoard");
-const eventLeaderboard = io.of("eventLeaderboard");
-//const chatRoom = io.of("chatRoom");
-
-// auth of socket
-//chatRoom.use(CheckTokenInSocket);
-notification.use(CheckTokenInSocket);
-
-leaderBoard.on('connection', GlobalLeaderBoard);
-eventLeaderboard.on('connection', LocalLeaderBoard);
-notification.on('connection', NotificationSocket);
-
-app.get('/', (req, res) => {
-    res.send('Notification server is running.');
-});
-
-server.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+StartServer();
