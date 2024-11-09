9b73636022ea8e1ed14c10c8ce83425882ad9eb2
aloknegi786 <aloksinghnegi005@gmail.com>
2024-11-09 15:39:57 +0530

    made the user profile card on dashboard functional

--- client/src/components/dashboard/cards/AboutUser.jsx
+++ client/src/components/dashboard/cards/AboutUser.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchuserProfile } from '../../../redux/features/user/userProfileSlice';
 
-const AboutUser = ({ user }) => {
+const y = ["year not-set","first year", "second year", "pre-final year", "final year"];
+
+const AboutUser = ({ name,username, email,year,branch, profilePic }) => {
+
   return (
     <div className="max-w-sm mx-auto overflow-hidden">
       <div className='flex items-center justify-center mt-5'>
         <img 
-          className="w-36 h-52 md:w-32 lg:w-48  border rounded-lg shadow-lg object-cover " 
-          src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg'
-          alt={`${user.name}'s profile`} 
+          className="w-36 h-52 md:w-32 lg:w-48 bg-blue-100  border rounded-lg shadow-lg object-cover " 
+          src={profilePic}
+          alt={`${name}'s profile`} 
         />
       </div>
       
       <div className="p-6 text-center shadow-lg">
-        <h2 className="text-2xl font-bold text-gray-800">{user.name}</h2>
-        <p className='text-xs text-gray-600'>{user.username}</p>
-        <p className="mt-2 text-gray-600">2nd year, Computer Science</p>
+        <h2 className="text-2xl font-bold text-gray-800">{name}</h2>
+        <p className='text-xs text-gray-600'>{username}</p>
+        <p className="mt-2 text-gray-600">{y[year]}, {branch == null ?"branch not-set":branch}</p>
       </div>
     </div>
   );
--- client/src/components/eventsSection/EventsPage.jsx
+++ client/src/components/eventsSection/EventsPage.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import EventCard from './EventCard';
+import { fetchEvents } from '../../redux/features/events/eventsSlice';
 
 function EventsPage() {
+  const dispatch = useDispatch();
+  const { events, status } = useSelector((state) => state.events);
+
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch]);
+
+  if (status === "loading") {
+    return <h2>Loading...</h2>;
+  }
+
+  if (status === "failed") {
+    return <h2>Failed to load data.</h2>;
+  }
+
   return (
     <div className="max-w-6xl mx-auto p-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+        {events.map((event,index) => (
+          <EventCard key={index} index={index} event={event} /> // Pass event as a prop
+        ))}
       </div>
     </div>
   );
